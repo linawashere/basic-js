214466e6109379f8707bde7560fe0e2d0ff55fd9
Polina <alllrrw@gmail.com>
2024-12-17 13:54:33 +0700

    add solutions

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,42 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  const resArr = [];
+  const length = arr.length;
+  let skipNext = false;
+
+  for (let i = 0; i < length; i++) {
+    const item = arr[i];
+
+    if (skipNext) {
+      skipNext = false; // Пропустим следующий элемент
+      continue;
+    }
+
+    if (item === '--discard-next') {
+      skipNext = true; // Установим флаг для пропуска
+      continue;
+    } 
+
+    if (item === '--discard-prev') {
+      if (resArr.length > 0) {
+        resArr.pop(); // Удаляем последний элемент, если он есть
+      }
+      continue;
+    }
+
+    // Проверяем, является ли элемент числом
+    if (typeof item === 'number') {
+      resArr.push(item); // Добавляем числовые элементы в результат
+    }
+  }
+
+  return resArr;
 }
 
 module.exports = {
