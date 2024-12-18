a60760f14cc1958da4f7b7834d7903f808a48904
Polina <alllrrw@gmail.com>
2024-12-18 18:39:03 +0700

    add solutions

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  const coincidence = [];
+  const s2Array = s2.split('');
+  s1.split('').forEach((item) => {
+    const index = s2Array.indexOf(item);
+    if (index !== -1) {
+      coincidence.push(item);
+      s2Array.splice(index, 1);
+    }
+  });
+  return coincidence.length;
 }
 
 module.exports = {
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,21 +20,59 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    let messToUpperCase = [];
-    if (!message || !key) {
-      throw new Error('Incorrect arguments!');
-    }
-    for (let i = 0; i < message.length; i++) {
-      if (message.charCodeAt(i) >= 97 && message.charCodeAt(i) >= 122) {
-        message[i] = message[i].toUpperCase();
-        messToUpperCase.push(message[i]);
-      } else {
-        messToUpperCase.push(message[i]);
+  encrypt(message, key) {
+    // let messToUpperCase = [];
+    // let keyToUpperCase = [];
+    // if (!message || !key) {
+    //   throw new Error('Incorrect arguments!');
+    // }
+    // for (let i = 0; i < message.length; i++) {
+    //   if (message.charCodeAt(i) >= 97 && message.charCodeAt(i) <= 122) {
+    //     message[i] = message[i].toUpperCase();
+    //     messToUpperCase.push(message[i]);
+    //   } else {
+    //     messToUpperCase.push(message[i]);
+    //   }
+    // }
+    // if (message.length > key.length) { //удлинение ключа
+    //   while (key.length !== message.length) {
+    //     for (const letter of key) {
+    //       key += letter;
+    //     }
+    //   }
+    // }
+    // for (let i = 0; i < key.length; i++) {
+    //   if (key.charCodeAt(i) >= 97 && key.charCodeAt(i) <= 122) {
+    //     key[i] = key[i].toUpperCase();
+    //     keyToUpperCase.push(key[i]);
+    //   } else {
+    //     keyToUpperCase.push(key[i]);
+    //   }
+    // }
+    // const numbersOfMessage = [];
+    // const numbersOfKey = [];
+    // for (let i = 0; i < messToUpperCase.length; i++) {
+    //   if (
+    //     messToUpperCase[i] >= 'A' && messToUpperCase[i] <= 'Z' ||
+    //     keyToUpperCase[i] >= 'A' && keyToUpperCase[i] <= 'Z'
+    //   ) {
+    //     numbersOfMessage.push(messToUpperCase.charCodeAt(i) - 64);
+    //     numbersOfKey.push(keyToUpperCase.charCodeAt(i) - 64);
+    //   }
+    // }
+    // const cipher = numbersOfMessage.map((item, index) =>{
+    //   return item + numbersOfKey[index];
+    // });
+    let encryptedMessage = '';
+    let keyLength = key.length;
+    for (let i = 0, j = 0; i < message.length; i++) {
+      let currentChar = message[i];
+      if (currentChar.match(/[a-zA-Z]/)) {
+        
       }
     }
   }
-  decrypt() {
+  decrypt(message, key) {
     let messToUpperCase = [];
     if (!message || !key) {
       throw new Error('Incorrect arguments!');
